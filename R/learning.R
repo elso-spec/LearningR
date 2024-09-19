bbadeeafd416913501f355f2afcf8cbcd5606bda
Elisabeth Solmunde <au600026@uni.au.dk>
2024-09-19 11:48:20 +0200

    summary statistics overall and by group

--- R/learning.R
+++ R/learning.R
@@ -107,16 +107,55 @@ nhanes_small %>%
 
 # 1. BMI between 20 and 40 with diabetes
 nhanes_small %>%
-    # Format should follow: variable >= number or character
-    filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes")
+  # Format should follow: variable >= number or character
+  filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes")
 
 # Pipe the data into mutate function and:
 nhanes_modified <- nhanes_small %>% # Specifying dataset
-    mutate(
-        # 2. Calculate mean arterial pressure
-        mean_arterial_pressure = ((2*bp_dia_ave) + bp_sys_ave)/3,
-        # 3. Create young_child variable using a condition
-        young_child = if_else(age < 6, "Yes", "No")
-    )
+  mutate(
+    # 2. Calculate mean arterial pressure
+    mean_arterial_pressure = ((2 * bp_dia_ave) + bp_sys_ave) / 3,
+    # 3. Create young_child variable using a condition
+    young_child = if_else(age < 6, "Yes", "No")
+  )
 
 nhanes_modified
+
+nhanes_small %>%
+  summarise(
+    max_bmi = max(bmi, na.rm = T),
+    min_bmi = min(bmi, na.rm = T)
+  )
+
+
+nhanes_small %>%
+  group_by(diabetes) %>%
+  summarise(
+    mean_age = mean(age, na.rm = T),
+    mean_bmi = mean(bmi, na.rm = T)
+  )
+
+nhanes_small %>%
+  filter(!is.na(diabetes)) %>%
+  group_by(diabetes) %>%
+  summarise(
+    mean_age = mean(age, na.rm = T),
+    mean_bmi = mean(bmi, na.rm = T)
+  )
+
+nhanes_small %>%
+  filter(!is.na(diabetes)) %>%
+  group_by(diabetes, phys_active) %>%
+  summarise(
+    mean_age = mean(age, na.rm = TRUE),
+    mean_bmi = mean(bmi, na.rm = TRUE)
+  )
+
+nhanes_small %>%
+  filter(!is.na(diabetes)) %>%
+  group_by(diabetes, phys_active) %>%
+  summarise(
+    mean_age = mean(age, na.rm = TRUE),
+    mean_bmi = mean(bmi, na.rm = TRUE)
+  ) %>%
+  ungroup()
